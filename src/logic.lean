b04fca293809e7d2af3d9a51cbd4e4f5a901fded
Isaac Lourenço <isaacmarlondasilvalourenco@gmail.com>
2022-09-01 00:23:37 -0300

    :tada: Solve forall_as_neg_exists

--- src/logic.lean
+++ src/logic.lean
@@ -359,7 +359,10 @@ end
 theorem forall_as_neg_exists :
   (∀x, P x) → ¬(∃x, ¬P x)  :=
 begin
-  sorry,
+  intros h h',
+  cases h' with x npx,
+  have px : P x := h x,
+  contradiction,
 end
 
 theorem forall_as_neg_exists_converse :
